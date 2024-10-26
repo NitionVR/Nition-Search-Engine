df2022c4cc815a38c3ab9b2034fcdbfe0860fce0
NitionVR <kmaganyane83@gmail.com>
2024-10-26 23:06:28 +0200

    Add two more test cases checking for non existent word and inserting the same word into different positions

--- src/test/java/nitionsearch/search/SuffixTrieTest.java
+++ src/test/java/nitionsearch/search/SuffixTrieTest.java
@@ -3,11 +3,10 @@ package nitionsearch.search;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SuffixTrieTest {
     private SuffixTrie trie;
 
@@ -23,6 +22,24 @@ public class SuffixTrieTest {
 
         assertEquals(1, positions.size());
         assertEquals(10, positions.get(0));
+    }
+
+    @Test
+    public void testSearchNonExistentWord(){
+        trie.insert("banana",5);
+        List<Integer> positions = trie.search("apple");
+        assertTrue(positions.isEmpty());
+    }
+
+    @Test
+    public void insertMultiplePositionsForSameWord(){
+        trie.insert("lemon",10);
+        trie.insert("lemon", 20);
+
+        List<Integer> positions = trie.search("lemon");
+        assertEquals(2, positions.size());
+        assertTrue(positions.contains(10));
+        assertTrue(positions.contains(20));
     }
 
 }

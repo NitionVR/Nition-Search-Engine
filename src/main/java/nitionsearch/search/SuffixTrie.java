82f40042849480a2768d206e34433aa045254385
NitionVR <kmaganyane83@gmail.com>
2024-10-26 22:53:58 +0200

    Implement the insert and search functionality of the suffix trie

--- src/main/java/nitionsearch/search/SuffixTrie.java
+++ src/main/java/nitionsearch/search/SuffixTrie.java
@@ -1,6 +1,30 @@
 package nitionsearch.search;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SuffixTrie {
-    private TrieNode root = new TrieNode();
+    private final TrieNode root = new TrieNode();
+
+    public void insert(String word, int position){
+        TrieNode node = root;
+
+        for (char c:word.toCharArray()){
+            node = node.children.computeIfAbsent(c, k -> new TrieNode());
+        }
+        node.positions.add(position);
+    }
+
+    public List<Integer> search(String word){
+        TrieNode node = root;
+
+        for (char c:word.toCharArray()){
+            node = node.children.get(c);
+            if (node == null)
+                return new ArrayList<>();
+        }
+        return node.positions;
+
+    }
 }

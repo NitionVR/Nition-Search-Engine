3b4edafd9c9b23f6ea0b2cb22d13faf5ac0876a3
NitionVR <kmaganyane83@gmail.com>
2024-10-27 17:42:18 +0200

    Implement the basic web crawler with url validation

--- src/main/java/nitionsearch/crawler/WebCrawler.java
+++ src/main/java/nitionsearch/crawler/WebCrawler.java
@@ -1,2 +1,32 @@
-package nitionsearch.crawler;public class WebCrawler {
+package nitionsearch.crawler;
+
+import nitionsearch.search.SearchEngine;
+import nitionsearch.model.Page;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class WebCrawler {
+    private final SearchEngine searchEngine;
+    private int nextPageId = 0;
+    public WebCrawler (SearchEngine searchEngine){
+        this.searchEngine = searchEngine;
+    }
+
+    public void crawlAndIndex(String url, String content){
+        if (!isValidURL(url)) {
+            throw new IllegalArgumentException("Invalid URL: " + url);
+        }
+        Page page = new Page(nextPageId++, url, content);
+        searchEngine.addPage(page);
+    }
+
+    public boolean isValidURL(String url) {
+        try {
+            URI.create(url).toURL();
+        } catch (MalformedURLException | IllegalArgumentException e) {
+            return false;
+        }
+
+        return true;
+    }
 }

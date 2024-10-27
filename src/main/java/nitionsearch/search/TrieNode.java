2c334a3302a0c338a17b8b95048e675a317d17d2
NitionVR <kmaganyane83@gmail.com>
2024-10-27 05:44:10 +0200

    Add a map to track occurrences by page ID and updated methods accordingly. Make children and occurrences fields private and add accessors.

--- src/main/java/nitionsearch/search/TrieNode.java
+++ src/main/java/nitionsearch/search/TrieNode.java
@@ -6,6 +6,23 @@ import java.util.List;
 import java.util.Map;
 
 public class TrieNode {
-    Map<Character, TrieNode> children = new HashMap<>();
-    List<Integer> positions = new ArrayList<>();
+
+
+    private Map<Character, TrieNode> children = new HashMap<>();
+    private Map<Integer, List<Integer>> occurrences = new HashMap<>();
+
+
+    public Map<Character, TrieNode> getChildren() {
+        return children;
+    }
+
+    public void addOccurrence(int pageId, int position) {
+        occurrences.computeIfAbsent(pageId, k -> new ArrayList<>()).add(position);
+    }
+
+    public Map<Integer, List<Integer>> getOccurrences() {
+        return occurrences;
+    }
+
+
 }
